54eaee1f19911a6392f5520e30bd5f8a0c90fde9
igor-kan <igorkan010@gmail.com>
2026-09-22 10:12:40 -0400

    fix: use compound assignment in merge_sort

--- sorting/merge_sort.rs
+++ sorting/merge_sort.rs
@@ -2,6 +2,6 @@ pub fn merge_sort(a: &[i64]) -> Vec<i64> {
     if a.len()<=1 { return a.to_vec(); }
     let m=a.len()/2; let (l,r)=(merge_sort(&a[..m]),merge_sort(&a[m..]));
     let (mut i,mut j,mut res)=(0,0,vec![]);
-    while i<l.len()&&j<r.len() { if l[i]<=r[j]{res.push(l[i]);i++;}else{res.push(r[j]);j++;} }
+    while i<l.len()&&j<r.len() { if l[i]<=r[j]{res.push(l[i]);i+=1;}else{res.push(r[j]);j+=1;} }
     res.extend_from_slice(&l[i..]); res.extend_from_slice(&r[j..]); res
 }
